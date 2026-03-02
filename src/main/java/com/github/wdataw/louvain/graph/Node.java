65cc0765903d84ec65dba5a11d220ef445468770
WdataW <wael272726@gmail.com>
2026-03-02 23:36:49 +0300

    feat: read and convert file to Graph object

--- src/main/java/com/github/wdataw/louvain/graph/Node.java
+++ src/main/java/com/github/wdataw/louvain/graph/Node.java
@@ -1,15 +1,17 @@
 package com.github.wdataw.louvain.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Node {
     //attributes
     private String nodeName;
     private final int nodeID;
-    private static int idCounter = 0;
-    
+
     //constructors
-    public Node(String nodeName) {
-        this.nodeID = ++idCounter;
-        this.nodeName = nodeName;
+    public Node(int nodeID) {
+        this.nodeID = nodeID;
     }
     
     //getters and setters
@@ -22,4 +24,33 @@ public class Node {
     public void setNodeName(String nodeName) {
         this.nodeName = nodeName;
     }
+
+    // equals
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return nodeID == node.nodeID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nodeID);
+    }
+
+    // extracts nodes from a set of edges
+    public static List<Node> extractNodes(List<Edge> edges){// KEY REMOVE
+        List<Node> nodes = new ArrayList<Node>();
+        for(Edge e:edges){
+            // add endpoints to the list of nodes only if they don't already exist in it
+            Node n1 = e.getEndpoints().getNode1();
+            if(!nodes.contains(n1))nodes.add(n1);
+            Node n2 = e.getEndpoints().getNode2();
+            if(!nodes.contains(n2))nodes.add(n2);
+        }
+        return nodes;
+    }
+
 }
