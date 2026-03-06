206c79e2088ca7bfec9818fc54cc98563d88de16
tkmyg12345 <86067941+tkmyg12345@users.noreply.github.com>
2026-03-07 02:34:21 +0300

    normalizing the comments in Endpoints class

--- src/main/java/com/github/wdataw/louvain/graph/Endpoints.java
+++ src/main/java/com/github/wdataw/louvain/graph/Endpoints.java
@@ -1,17 +1,21 @@
 package com.github.wdataw.louvain.graph;
 
 public class Endpoints {
+    
     // attributes
+    
     private Node node1;
     private Node node2;
 
     // constructor
+    
     public Endpoints(Node node1, Node node2) {
         this.node1 = node1;
         this.node2 = node2;
     }
 
     // getters
+    
     public Node getNode1() {
         return node1;
     }
@@ -20,6 +24,7 @@ public class Endpoints {
     }
 
     // setters
+    
     public void setNode1(Node node1) {
         this.node1 = node1;
     }
