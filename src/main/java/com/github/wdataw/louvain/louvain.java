f91c1ae1f788870e2026dd6602872b681eee5f32
tkmyg12345 <86067941+tkmyg12345@users.noreply.github.com>
2026-03-09 18:55:38 +0300

    Create louvain.java

---
+++ src/main/java/com/github/wdataw/louvain/louvain.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.github.wdataw.louvain;
+
+/**
+ *
+ * @author Tkmyg12345
+ */
+public class louvain {
+    
+}
