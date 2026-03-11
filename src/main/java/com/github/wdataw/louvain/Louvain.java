5f4d8815ccf089fc64898c4a9a4ba8b75f0d5306
tkmyg12345 <86067941+tkmyg12345@users.noreply.github.com>
2026-03-12 02:36:05 +0300

    ChatGPT implementation of the method

--- src/main/java/com/github/wdataw/louvain/Louvain.java
+++ src/main/java/com/github/wdataw/louvain/Louvain.java
@@ -1,5 +1,8 @@
 package com.github.wdataw.louvain;
-import com.github.wdataw.louvain.graph.Graph;
+import com.github.wdataw.louvain.graph.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Louvain {
@@ -19,5 +22,65 @@ public class Louvain {
             modularity += (1/twoM) * (sigmaC - (sigmaCHat * sigmaCHat) / twoM);
         }
         return modularity;
+    }
+
+
+    public Partition optimize(Graph graph, Partition communities) {
+
+        boolean improvement = true;
+
+        while (improvement) {
+
+            improvement = false;
+
+            for (Node node : graph.getNodes()) {
+
+                int originalCommunity = communities.communityOf(node);
+                double bestModularity = modularityOf(graph, communities);
+                int bestCommunity = originalCommunity;
+
+                // collect neighbor communities
+                Set<Integer> neighborCommunities = new HashSet<>();
+
+                for (Edge e : graph.getAdjList().get(node.getNodeId())) {
+
+                    Node n1 = e.getEndpoints().getNode1();
+                    Node n2 = e.getEndpoints().getNode2();
+
+                    Node neighbor = n1.equals(node) ? n2 : n1;
+
+                    neighborCommunities.add(communities.communityOf(neighbor));
+                }
+
+                // try moving node to each neighbor community
+                for (int candidateCommunity : neighborCommunities) {
+
+                    if (candidateCommunity == originalCommunity)
+                        continue;
+
+                    // move node
+                    communities.moveNodeToCommunity(node, candidateCommunity);
+
+                    double newModularity = modularityOf(graph, communities);
+
+                    if (newModularity > bestModularity) {
+                        bestModularity = newModularity;
+                        bestCommunity = candidateCommunity;
+                    }
+
+                    // move back
+                    communities.moveNodeToCommunity(node, originalCommunity);
+                }
+
+                // apply best move
+                if (bestCommunity != originalCommunity) {
+
+                    communities.moveNodeToCommunity(node, bestCommunity);
+                    improvement = true;
+                }
+            }
+        }
+
+        return communities;
     }
 }
