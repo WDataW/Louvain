bd542fde1c3824461b7d74cb4b28ae8621c41335
WdataW <wael272726@gmail.com>
2026-03-13 02:56:32 +0300

    feat/gen-node-positions

--- src/main/java/com/github/wdataw/louvain/Main.java
+++ src/main/java/com/github/wdataw/louvain/Main.java
@@ -12,8 +12,8 @@ public class Main {
         Locale.setDefault(Locale.US);// essential don't remove.
 //        Graph exampleGraph = Graph.readGraph("/p2p-Gnutella05.txt","\t");// 8846 nodes, 31839 edges
 //        Graph exampleGraph = Graph.readGraph("/p2p-Gnutella08.txt","\t");// 6301 nodes, 20777 edges
-//        Graph exampleGraph = Graph.readGraph("/email-Eu-core.txt"," ");// 1005 nodes, 25571 edges
-        Graph exampleGraph = Graph.getExample();// 4 nodes, 5 edges
+        Graph exampleGraph = Graph.readGraph("/email-Eu-core.txt"," ");// 1005 nodes, 25571 edges
+//        Graph exampleGraph = Graph.getExample();// 4 nodes, 5 edges
         Partition communities = new Partition(exampleGraph);
         JSONExporter.toJSON(exampleGraph,communities,"visualization/public/initialGraph.json");
     }
--- src/main/java/com/github/wdataw/louvain/graph/JSONExporter.java
+++ src/main/java/com/github/wdataw/louvain/graph/JSONExporter.java
@@ -1,12 +1,44 @@
     package com.github.wdataw.louvain.graph;
 
     import com.github.wdataw.louvain.Partition;
+    import org.jgrapht.alg.drawing.FRLayoutAlgorithm2D;
+    import org.jgrapht.alg.drawing.model.Box2D;
+    import org.jgrapht.alg.drawing.model.MapLayoutModel2D;
+    import org.jgrapht.alg.drawing.model.Point2D;
+    import org.jgrapht.graph.DefaultEdge;
+    import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
 
     import java.io.FileNotFoundException;
     import java.io.PrintWriter;
     import java.util.List;
 
     public class JSONExporter {
+        private static Point2D[] computePositions(Graph graph){
+            org.jgrapht.Graph<String, DefaultEdge> JGraph = new DefaultUndirectedWeightedGraph<>(DefaultEdge.class);
+            for (Node n: graph.getNodes()){
+                JGraph.addVertex(stringify(n.getNodeId()));
+            }
+            for(Edge e: graph.getEdges()){
+                String node1Id = stringify(e.getEndpoints().getNode1().getNodeId());
+                String node2Id = stringify(e.getEndpoints().getNode2().getNodeId());
+                JGraph.addEdge(node1Id,node2Id);
+            }
+            FRLayoutAlgorithm2D<String, DefaultEdge> layout = new FRLayoutAlgorithm2D<>(100); // bump to 500+
+            MapLayoutModel2D<String> model = new MapLayoutModel2D<>(new Box2D(5000, 5000));
+            layout.layout(JGraph, model);
+
+            Point2D[] positions = new Point2D[graph.getNodes().size()];
+            List<Node> nodes = graph.getNodes();
+            for (int i = 0; i < nodes.size(); i++) {
+                Point2D pos = model.get(stringify(nodes.get(i).getNodeId()));
+                positions[i] = pos;
+            }
+            return positions;
+        }
+        private static String stringify(int i){
+            return String.valueOf(i);
+        }
+
         public static void toJSON(Graph graph, Partition communities, String destination){
             PrintWriter jsonFile = null;
             try{
@@ -18,9 +50,11 @@
 
             jsonFile.println("{");// start of JSON file
             jsonFile.println("\"nodes\":[");// start of nodes array
+            Point2D[] positions = computePositions(graph);
             List<Node> nodes = graph.getNodes();
             for(Node n: nodes){
-                String item = String.format("{\"id\":%d,\"label\":\"%d\",\"group\":%d}",n.getNodeId(), n.getNodeId(),communities.communityOf(n));
+                int nodeId = n.getNodeId();
+                String item = String.format("{\"id\":%d,\"label\":\"%d\",\"group\":%d,\"x\":%f,\"y\":%f}",nodeId, nodeId,communities.communityOf(n), positions[nodeId].getX(), positions[nodeId].getY());
                 if(!n.equals(nodes.getLast())) item+=",";
                 jsonFile.println(item);
             }
