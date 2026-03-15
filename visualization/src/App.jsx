0feb6bd63485a4e0431ea2b9d07abeed2321909f
WdataW <wael272726@gmail.com>
2026-03-15 20:06:47 +0300

    fix: graph page UI for mobile

--- visualization/src/App.jsx
+++ visualization/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import LandingPage from "./pages/LandingPage";
 import GraphPage from "./pages/GraphPage";
 import { BrowserRouter, Route, Routes } from "react-router";
+import NotFound from "./pages/NotFound";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
             <Routes>
                 <Route index element={<LandingPage />} />
                 <Route path="/visualize/:dirName" element={<GraphPage />} />
+                <Route path="/*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     </div>
