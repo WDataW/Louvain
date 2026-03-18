227c656723470ddb219ad931210c5d672d61732a
WdataW <wael272726@gmail.com>
2026-03-18 21:05:10 +0300

    clean Endpoints.java

--- src/main/java/com/github/wdataw/louvain/graph/Endpoints.java
+++ src/main/java/com/github/wdataw/louvain/graph/Endpoints.java
@@ -2,8 +2,8 @@ package com.github.wdataw.louvain.graph;
 
 public class Endpoints {
     // attributes
-    private Node node1;
-    private Node node2;
+    private final Node node1;
+    private final Node node2;
 
     // constructor
     public Endpoints(Node node1, Node node2) {
@@ -17,13 +17,5 @@ public class Endpoints {
     }
     public Node getNode2() {
         return node2;
-    }
-
-    // setters
-    public void setNode1(Node node1) {
-        this.node1 = node1;
-    }
-    public void setNode2(Node node2) {
-        this.node2 = node2;
     }
 }
