2ac6bc6191429e1ce36399ba08d9b8001f0e81f0
WdataW <wael272726@gmail.com>
2026-03-18 21:04:00 +0300

    clean Edge.java

--- src/main/java/com/github/wdataw/louvain/graph/Edge.java
+++ src/main/java/com/github/wdataw/louvain/graph/Edge.java
@@ -1,24 +1,15 @@
 package com.github.wdataw.louvain.graph;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.List;
 import java.util.Objects;
-import java.util.Scanner;
 
 public class Edge {
     // attributes
     private static int idCounter = 0;
     private final int edgeID;
     private double edgeWeight;
-    private Endpoints endpoints;
+    private final Endpoints endpoints;
 
     // constructors
-    public Edge(Endpoints endpoints ,double edgeWeight) {
-        this.edgeWeight = edgeWeight;
-        this.endpoints = new Endpoints(endpoints.getNode1(), endpoints.getNode2());
-        this.edgeID = idCounter++;
-    }
     public Edge(Node node1, Node node2 ,double EdgeWeight) {
         this.edgeWeight = EdgeWeight;
         this.endpoints = new Endpoints(node1,node2);
@@ -29,14 +20,6 @@ public class Edge {
     public void setEdgeWeight(double edgeWeight) {
         this.edgeWeight = edgeWeight;
     }
-    public void setEndpoints(Endpoints endpoints){
-        this.endpoints.setNode1(endpoints.getNode1());
-        this.endpoints.setNode2(endpoints.getNode2());
-    }
-    public void setEndpoints(Node node1, Node node2){
-        this.endpoints.setNode1(node1);
-        this.endpoints.setNode2(node2);
-    }
 
     // getters
     public int getEdgeID() {
@@ -46,7 +29,6 @@ public class Edge {
         return edgeWeight;
     }
     public Endpoints getEndpoints(){ return endpoints; }
-
 
     @Override
     public boolean equals(Object o) {
