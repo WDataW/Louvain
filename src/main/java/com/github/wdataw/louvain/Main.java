c2997b6e19c9bd477571d73c95d2e682f895a42c
WdataW <wael272726@gmail.com>
2026-03-19 06:00:43 +0300

    replace hamstrer faulty JSONs

--- src/main/java/com/github/wdataw/louvain/Main.java
+++ src/main/java/com/github/wdataw/louvain/Main.java
@@ -10,10 +10,10 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
-
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);// essential don't remove.
+<<<<<<< Updated upstream
 //        Graph graph = Graph.readGraph("/p2p-Gnutella31.txt","\t");// 62586 nodes, 147892 edges
 //        Graph graph = Graph.readGraph("/deezer_europe.csv",",");// 28281 nodes, 92752 edges
 //        Graph graph = Graph.readGraph("/facebook_combined.txt"," ");// 4039 nodes, 88234 edges
@@ -26,11 +26,29 @@ public class Main {
             Map<Integer, Set<Integer>> currentLevel = dendrogram.get(i);
             System.out.println("Level "+i);
             for(int j:currentLevel.keySet()){
+=======
+        // Graph graph = Graph.readGraph("/p2p-Gnutella31.txt", "\t");// 62586 nodes,
+        // 147892 edges
+        // Graph graph = Graph.readGraph("/deezer_europe.csv",",");// 28281 nodes, 92752
+        // edges
+        // Graph graph = Graph.readGraph("/facebook_combined.txt"," ");// 4039 nodes,
+        // 88234 edges
+        Graph graph = Graph.readGraph("/BioGrid-Arabidopsis-Thaliana-Columbia.txt", " ");
+        // Graph graph = Graph.readGraph("/petster-friendships-hamster-uniq", " ");
+        // Graph graph = Graph.readGraph("/video-example.txt", " ");
+        List<Map<Integer, Set<Integer>>> dendrogram = Louvain.louvain(graph, "test");
+
+        // print dendogram
+        for (int i = 0; i < dendogram.size(); i++) {
+            Map<Integer, Set<Integer>> currentLevel = dendogram.get(i);
+            System.out.println("Level " + i);
+            for (int j : currentLevel.keySet()) {
+>>>>>>> Stashed changes
                 int superNodeId = j;
                 Set<Integer> originalNodeIds = currentLevel.get(superNodeId);
-                System.out.print(superNodeId+ " -> ");
-                for(int k:originalNodeIds)
-                    System.out.print(k+" ");
+                System.out.print(superNodeId + " -> ");
+                for (int k : originalNodeIds)
+                    System.out.print(k + " ");
                 System.out.println();
             }
             System.out.println();
